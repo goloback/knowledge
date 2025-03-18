e31426f87c74b1d50accdc790363c2fb527755ba
goloback <adduma2008@gmail.com>
2025-03-18 16:02:31 +0200

    update

--- lib/main.dart
+++ lib/main.dart
@@ -8,32 +8,38 @@ void main() {
   runApp(my_app);
 }
 
-
-
 class Math extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('knowledge', style: TextStyle(backgroundColor: const Color.fromARGB(255, 16, 102, 200)),),
-          backgroundColor: Color.fromARGB(255, 16, 102, 200),
-          ),
-        body: Function_screen(),
-      ),
+      home:FirstPage(), 
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class Function_screen extends StatefulWidget{
+class FirstPage extends StatefulWidget{
   @override
-  Function_screen_ createState() {
-    return Function_screen_();
+  State<StatefulWidget> createState() {
+    return _FirstPage();
   }
 }
 
-class Function_screen_ extends State<Function_screen>{
+class _FirstPage extends State<FirstPage>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+
+class Function_screen extends StatefulWidget{
+  @override
+  _Function_screen createState() {
+    return _Function_screen();
+  }
+}
+
+class _Function_screen extends State<Function_screen>{
   String answer = 'Your answer';
   var bgplus = Colors.green;
   var bgminus =  Colors.grey;
@@ -214,90 +220,96 @@ void createNextQuest(){
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: [
-        Row(
-          children: [
-            ElevatedButton(onPressed: clickPlus, style: ElevatedButton.styleFrom(backgroundColor: bgplus),
-             child: Text('+', style: TextStyle(fontSize: 25),),),
-            ElevatedButton(onPressed: clickMinus, style: ElevatedButton.styleFrom(backgroundColor: bgminus),
-             child: Text('-', style: TextStyle(fontSize: 25),)),
-            ElevatedButton(onPressed: clicktimes, style: ElevatedButton.styleFrom(backgroundColor: bgtimes),
-            child: Text('*', style: TextStyle(fontSize: 25),)),
-            ElevatedButton(onPressed: clickdivide, style: ElevatedButton.styleFrom(backgroundColor: bgdivide),
-            child: Text('/', style: TextStyle(fontSize: 25),)),
-          ],
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        ),
-        Row(
-          children: [
-            Container(
-              padding: EdgeInsets.symmetric(vertical: 50,horizontal: 80),
-              child: 
-                Text('$number1 $symbol $number2', 
-                  style: TextStyle(fontSize: 25),
-                ),
-              decoration: BoxDecoration(color: colors_quest, borderRadius: BorderRadius.circular(15)),
-            )
-          ],
-          mainAxisAlignment: MainAxisAlignment.center,
-          
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-          Text(answer,
-              style: TextStyle(
-                fontSize: 25,
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('knowledge', style: TextStyle(backgroundColor: const Color.fromARGB(255, 16, 102, 200)),),
+          backgroundColor: Color.fromARGB(255, 16, 102, 200),
+          ),
+        body:  Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Row(
+            children: [
+              ElevatedButton(onPressed: clickPlus, style: ElevatedButton.styleFrom(backgroundColor: bgplus),
+              child: Text('+', style: TextStyle(fontSize: 25),),),
+              ElevatedButton(onPressed: clickMinus, style: ElevatedButton.styleFrom(backgroundColor: bgminus),
+              child: Text('-', style: TextStyle(fontSize: 25),)),
+              ElevatedButton(onPressed: clicktimes, style: ElevatedButton.styleFrom(backgroundColor: bgtimes),
+              child: Text('*', style: TextStyle(fontSize: 25),)),
+              ElevatedButton(onPressed: clickdivide, style: ElevatedButton.styleFrom(backgroundColor: bgdivide),
+              child: Text('/', style: TextStyle(fontSize: 25),)),
+            ],
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          ),
+          Row(
+            children: [
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 50,horizontal: 80),
+                child: 
+                  Text('$number1 $symbol $number2', 
+                    style: TextStyle(fontSize: 25),
+                  ),
+                decoration: BoxDecoration(color: colors_quest, borderRadius: BorderRadius.circular(15)),
+              )
+            ],
+            mainAxisAlignment: MainAxisAlignment.center,
+            
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+            Text(answer,
+                style: TextStyle(
+                  fontSize: 25,
 
-              ),)
-        ],
-        ),
+                ),)
+          ],
+          ),
 
-        Column(
-          children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton(onPressed: ()=>clickNumber('1'), child: Text('1', style: TextStyle(fontSize: 25),)),
-                  ElevatedButton(onPressed: ()=>clickNumber('2'), child: Text('2', style: TextStyle(fontSize: 25),)),
-                  ElevatedButton(onPressed: ()=>clickNumber('3'), child: Text('3', style: TextStyle(fontSize: 25),)),
-    
-                 ],
+          Column(
+            children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(onPressed: ()=>clickNumber('1'), child: Text('1', style: TextStyle(fontSize: 25),)),
+                    ElevatedButton(onPressed: ()=>clickNumber('2'), child: Text('2', style: TextStyle(fontSize: 25),)),
+                    ElevatedButton(onPressed: ()=>clickNumber('3'), child: Text('3', style: TextStyle(fontSize: 25),)),
+      
+                  ],
+                  ), 
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(onPressed: ()=>clickNumber('4'), child: Text('4', style: TextStyle(fontSize: 25),)),
+                    ElevatedButton(onPressed: ()=>clickNumber('5'), child: Text('5', style: TextStyle(fontSize: 25),)),
+                    ElevatedButton(onPressed: ()=>clickNumber('6'), child: Text('6', style: TextStyle(fontSize: 25),)),
+            
+                  ],
                 ), 
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton(onPressed: ()=>clickNumber('4'), child: Text('4', style: TextStyle(fontSize: 25),)),
-                  ElevatedButton(onPressed: ()=>clickNumber('5'), child: Text('5', style: TextStyle(fontSize: 25),)),
-                  ElevatedButton(onPressed: ()=>clickNumber('6'), child: Text('6', style: TextStyle(fontSize: 25),)),
-          
-                ],
-              ), 
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton(onPressed: ()=>clickNumber('7'), child: Text('7', style: TextStyle(fontSize: 25),)),
-                  ElevatedButton(onPressed: ()=>clickNumber('8'), child: Text('8', style: TextStyle(fontSize: 25),)),
-                  ElevatedButton(onPressed: ()=>clickNumber('9'), child: Text('9', style: TextStyle(fontSize: 25),)),
-          
-                ],
-              ), 
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton(onPressed: ()=>clickNumber('0'), child: Text('0', style: TextStyle(fontSize: 25),)),
-                  ElevatedButton(onPressed: changePlusMinus, child: Text('+/-', style: TextStyle(fontSize: 25),)),
-                  ElevatedButton(onPressed: deleteNumber, child: Text('x', style: TextStyle(fontSize: 25),)),
-          
-                ],
-              ), 
-          ],
-       )
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(onPressed: ()=>clickNumber('7'), child: Text('7', style: TextStyle(fontSize: 25),)),
+                    ElevatedButton(onPressed: ()=>clickNumber('8'), child: Text('8', style: TextStyle(fontSize: 25),)),
+                    ElevatedButton(onPressed: ()=>clickNumber('9'), child: Text('9', style: TextStyle(fontSize: 25),)),
+            
+                  ],
+                ), 
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(onPressed: ()=>clickNumber('0'), child: Text('0', style: TextStyle(fontSize: 25),)),
+                    ElevatedButton(onPressed: changePlusMinus, child: Text('+/-', style: TextStyle(fontSize: 25),)),
+                    ElevatedButton(onPressed: deleteNumber, child: Text('x', style: TextStyle(fontSize: 25),)),
+            
+                  ],
+                ), 
+            ],
+        )
 
       
-      ],
-    );
+        ],
+        ),
+      );
   }
 }
